aab1e441e7c3e77d8acd10bd03f1e8d672fde9e3
sidgampa1 <siddharth_gampa@yahoo.com>
2023-11-10 22:15:56 -0800

    Updated reviews.controller.js import path

--- api/reviews.controller.js
+++ api/reviews.controller.js
@@ -1,4 +1,4 @@
-import ReviewsDAO from "../dao/ReviewsDAO.js"
+import ReviewsDAO from "../dao/reviewsDAO.js"
 
 export default class ReviewsController {
     static async apiPostReview(req, res, next) {
